9f6e3065f31750e3fec4ce6369d2b7cfbeea1c90
FabianaMP <fmarquespenteado@gmail.com>
2024-05-07 16:53:34 -0300

    first commit

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       //Conhecer e importar a classe scanner
+       Scanner scanner = new Scanner(System.in); 
+
+       //Exibir as mensagesn para o nosso usuario
+       System.out.println("Por favor, insira o seu nome:"); 
+
+       // Obter pelo scanner os valores digitados no terminal
+       String nome = scanner.nextLine();
+
+       System.out.println("Por favor, insira o valor inicial da conta:");
+       double valorInicial = scanner.nextDouble();
+
+       // Exibimos a mensagem conta criada 
+       System.out.println("Conta criada com sucesso! Nome: " + nome + ", Valor Inicial: " + valorInicial);
+        
+       // Fechamos o scanner para evitar vazamentos de memória
+       scanner.close(); 
+
     }
 }
